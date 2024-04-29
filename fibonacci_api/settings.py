061f3c39f6ac9fec1e2005bcb0c4958a2a43d9de
ryunosuke-ishiguro <ryunosuke_ishiguro@demia.co.jp>
2024-04-29 16:26:53 +0900

    settings.pyの修正

--- fibonacci_api/settings.py
+++ fibonacci_api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECREST_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'fibonacci_api.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'guro_fibonacci_api.onrender.com']
 
 
 # Application definition
