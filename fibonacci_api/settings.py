c91d2bcc84b2678cff18619a63e32fb2fa97e504
ryunosuke-ishiguro <ryunosuke_ishiguro@demia.co.jp>
2024-05-01 16:28:53 +0900

    render.yamlの修正

--- fibonacci_api/settings.py
+++ fibonacci_api/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['127.0.0.1', 'fibonacci_api.onrender.com']
 
 
 # Application definition
@@ -112,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ja"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Tokyo"
 
 USE_I18N = True
 
